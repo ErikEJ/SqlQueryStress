f1e8422149db7242498754d5bb737f712adad6e5
ErikEJ <ejlskov@hotmail.com>
2016-03-03 08:20:23 +0100

    Add conn.Close back

--- src/SQLQueryStress/LoadEngine.cs
+++ src/SQLQueryStress/LoadEngine.cs
@@ -462,6 +462,7 @@ namespace SQLQueryStress
                                 //Clean up the connection
                                 if (_statsComm != null)
                                     conn.InfoMessage -= handler;
+                                conn.Close();
                             }
 
                             var finished = i == _iterations - 1;
