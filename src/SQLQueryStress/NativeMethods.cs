37a1268a262562306869f539df990c2a8adaae52
Mert YETER <46247241+mertyeter@users.noreply.github.com>
2020-09-07 13:13:33 +0300

    Updated CommandLineParser to latest version (2.8.0)

---
+++ src/SQLQueryStress/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SQLQueryStress
+{
+    internal static class NativeMethods
+    {
+        private const int ATTACH_PARENT_PROCESS = -1;
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern bool AttachConsole(int processId);
+
+        internal static bool AttachParentConsole() => AttachConsole(ATTACH_PARENT_PROCESS);
+    }
+}
