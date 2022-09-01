186f570c4dd656d17159b4e119e3b9a475422f6a
fred <domaine75@gmail.com>
2022-09-01 11:01:08 +0200

    Fix labels which stop compilation

--- src/SQLQueryStress/Options.Designer.cs
+++ src/SQLQueryStress/Options.Designer.cs
@@ -71,23 +71,23 @@ namespace SQLQueryStress
       // 
       // connection_timeout_label
       // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Connection Timeout";
+            this.connection_timeout_label.AutoSize = true;
+            this.connection_timeout_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connection_timeout_label.Location = new System.Drawing.Point(3, 17);
+            this.connection_timeout_label.Name = "label1";
+            this.connection_timeout_label.Size = new System.Drawing.Size(120, 13);
+            this.connection_timeout_label.TabIndex = 3;
+            this.connection_timeout_label.Text = "Connection Timeout";
       // 
       // command_timeout_label
       // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 17);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(196, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Command Timeout (0 = Unlimited)";
+            this.command_timeout_label.AutoSize = true;
+            this.command_timeout_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.command_timeout_label.Location = new System.Drawing.Point(3, 17);
+            this.command_timeout_label.Name = "label2";
+            this.command_timeout_label.Size = new System.Drawing.Size(196, 13);
+            this.command_timeout_label.TabIndex = 5;
+            this.command_timeout_label.Text = "Command Timeout (0 = Unlimited)";
       // 
       // connectionPooling_checkBox
       // 
