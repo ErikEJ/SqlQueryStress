15182ce7803585e21e3527051efdc53908eddf05
Erik Ejlskov Jensen <eej@delegate.dk>
2025-10-25 08:26:24 +0200

    add semi colons

--- querysettings.sql
+++ querysettings.sql
@@ -1,11 +1,11 @@
 -- Default SQL Server Management Studio query execution settings
 -- These settings match the default SSMS configuration for query execution
 -- ANSI settings
-SET QUOTED_IDENTIFIER ON
-SET ANSI_NULL_DFLT_ON ON
-SET ANSI_PADDING ON
-SET ANSI_WARNINGS ON
-SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_NULL_DFLT_ON ON;
+SET ANSI_PADDING ON;
+SET ANSI_WARNINGS ON;
+SET ANSI_NULLS ON;
 -- Advanced settings
 SET ARITHABORT ON;
 SET CONCAT_NULL_YIELDS_NULL ON;
